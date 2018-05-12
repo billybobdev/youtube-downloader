5c24170986ae9da7521da0c955559091829b1893
Bill Surgenor <billybobdev@protonmail.com>
2018-05-12 23:34:05 +0100

    Enable auto updating ( hopefully ) and tag v0.0.1

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,14 @@
-import { app, BrowserWindow } from 'electron' // eslint-disable-line
+import { app, BrowserWindow } from 'electron'; // eslint-disable-line import/no-extraneous-dependencies
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+import { autoUpdater } from 'electron-updater' // eslint-disable-line
 
 /**
  * Set `__static` path to static files in production
@@ -44,22 +54,11 @@ app.on('activate', () => {
   }
 });
 
-/**
- * Auto Updater
- *
- * Uncomment the following code below and install `electron-updater` to
- * support auto updating. Code Signing with a valid certificate is required.
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
- */
-
-/*
-import { autoUpdater } from 'electron-updater'
-
 autoUpdater.on('update-downloaded', () => {
-  autoUpdater.quitAndInstall()
-})
+  autoUpdater.quitAndInstall();
+});
 
 app.on('ready', () => {
-  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
-})
- */
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates();
+});
+
